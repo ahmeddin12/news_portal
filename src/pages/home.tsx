536bae3c5cdb6c2aeadbdfe0292736f44d5bb46f
ahmeddin12 <amudi621@gmail.com>
2025-10-26 07:12:11 +0300

    feat: add grid layout for additional cards in Home component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography, Button, Card, CardContent } from "@mui/material";
+import Grid2 from "@mui/material/Grid";
 
 import type { FC } from "react";
 
@@ -75,6 +76,31 @@ const Home: FC = () => {
           </Box>
         </CardContent>
       </Card>
+
+      <Box className="grid xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3 mt-8">
+        {[...Array(4)].map((item, ind) => (
+          <Grid2 key={ind}>
+            <Card className="relative">
+              <Box className="bg-red-300 w-full aspect-[16/10]"></Box>
+              <Box
+                className="_carouselGradient"
+                sx={{
+                  position: "absolute",
+                  bottom: 0,
+                  height: "70px",
+                  width: "100%",
+                }}
+              />
+              <Typography className="sm:text-xl text-lg font-serif line-clamp-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </Typography>
+            </Card>
+          </Grid2>
+        ))}
+      </Box>
     </Box>
   );
 };
