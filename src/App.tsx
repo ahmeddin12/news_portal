e9c92e28586b3c7b4f7583efd6855ca4b0f02fd9
ahmeddin12 <amudi621@gmail.com>
2025-11-07 00:39:23 +0300

    updated the home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { Box } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Carousel from "./components/Carousel";
+import { categories } from "./utils/constants";
+import HomeCardsList from "./components/HomeCardsList";
+
 import Navbar from "./components/Navbar";
 // import Home from "./pages/Home";
 import Explore from "./pages/Explore";
@@ -12,7 +16,17 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Box sx={{ width: "90%", mx: "auto" }}>
+            <Carousel />
+            <Box className="mt-4 mb-24">
+              <Box>
+                {categories.map((item: string, ind: number) => (
+                  <HomeCardsList key={ind} category={item} />
+                ))}
+              </Box>
+            </Box>
+          </Box>
+
           <Route path="/explore" element={<Explore />} />
           <Route path="/search" element={<Search />} />
         </Routes>
